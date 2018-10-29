c20b3294b04e2ee6c6fae9049f078058eaa97113
Erin Green <erin.y.green@gmail.com>
2018-10-29 18:43:34 +0000

    Nokogiri edits

--- lib/cli/project/scraper.rb
+++ lib/cli/project/scraper.rb
@@ -10,17 +10,28 @@ class Scraper
     schedule = Nokogiri::HTML(open(url))
     
     neighborhoods = []
+    parades = []
     
-    schedule.css("div.pageSchedule").each do |clickable|
-      clickable.css("ul.clickable").each do |parade|
-        binding.pry 
+    schedule.css("ul.parades").each do |block|
+      block.css("li.clickable").each do |parade|
         title = parade.css("span.parade a").text 
         neighborhood = parade.css("li.listing-location").text 
         time = parade.css("span.time").text 
-        date = parade.css("div.dates").text 
-      binding.pry 
-      end 
+      end
     end 
+    
+    schedule.css("div.pageSchedule").each do |block|
+      block.css("ul.parades").each do |parade|
+        neighborhood = parade.css("li.listing-location").text 
+        binding.pry
+      end
+    end 
+      
+      schedule.css("div.dates-wrapper").each do |date|
+         date = date.css("div.dates").text 
+      end 
+      binding.pry 
+      
   end
 end
 
